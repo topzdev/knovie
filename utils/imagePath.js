4d85c86f0d8578bb2fcf440ed3018068fd360b9a
unknown <christianlugod05@gmail.com>
2020-01-16 09:54:24 -0800

    [Deployment] Image path removed env replace link

--- utils/imagePath.js
+++ utils/imagePath.js
@@ -1,5 +1,3 @@
-const img_size = "original";
-require("dotenv").config();
 import not_found from "~/assets/img/not_found.png";
 
 export default (img_path, size) => {
@@ -7,5 +5,5 @@ export default (img_path, size) => {
     return not_found;
   }
 
-  return process.env.TMDB_IMAGE_PATH + size + img_path;
+  return "http://image.tmdb.org/t/p/" + size + img_path;
 };
