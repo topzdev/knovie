b09a83e31988e13c9eb0e89f82421e31e3ffc79d
topzdev <christianlugod05@gmail.com>
2023-06-26 13:44:40 +0800

    lazy image width and height

--- components/image/LazyImage.vue
+++ components/image/LazyImage.vue
@@ -4,13 +4,15 @@
     v-lazy="imagePath(path, size)"
     draggable="false"
     loading="lazy"
+    :height="height"
+    :width="width"
   />
 </template>
 
 <script>
 import imagePath from "@/utils/imagePath";
 export default {
-  props: ["path", "size"],
+  props: ["path", "size", "width", "height"],
   methods: { imagePath }
 };
 </script>
